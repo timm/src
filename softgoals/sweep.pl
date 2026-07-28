b00b64cff216fa418d8bc284ea38125ca4999906
timm <timm@ieee.org>
2026-07-27 21:27:52 -0400

    softgoals: sweep means over 20 repeats of best-of-100

--- softgoals/sweep.pl
+++ softgoals/sweep.pl
@@ -2,7 +2,7 @@
 % Scores: soft = satisficed softgoals / softgoals (want 1);
 % cost = leaves assumed satisfied / leaves (want 0);
 % d = sqrt((1-soft)^2 + cost^2)/sqrt(2), 0..1, lower better.
-% usage: swipl sweep.pl models/CSServices.pl   (N=256 worlds)
+% usage: swipl sweep.pl models/CSServices.pl   (20 repeats of best-of-100)
 :- ['nfr2.pl'].
 :- assertz(greedy).
 :- initialization(main, main).
@@ -27,13 +27,22 @@ d(K,D,Dist) :- aggregate_all(count,node(_,softgoal),NS),
                S is K/NS, C is D/NL,
                Dist is sqrt((1-S)^2 + C^2)/sqrt(2).
 
-main :- set_random(seed(1)), statistics(cputime,T0),
-        findall(K-D, (between(1,256,_), world(K,D)), Ws),
-        statistics(cputime,T1), T is T1-T0,
+best100(B,SP,CP) :-
+        findall(K-D, (between(1,100,_), world(K,D)), Ws),
         findall(Dist-(K-D),(member(K-D,Ws),d(K,D,Dist)),Sc),
         msort(Sc,[B-(BK-BD)|_]),
         aggregate_all(count,node(_,softgoal),NS),
         aggregate_all(count,leaf(_),NL),
-        SP is 100*BK/NS, CP is 100*BD/NL,
-        format("decisions ~w | ~1fs | d ~3f | soft ~w/~w (~1f%) | cost ~w/~w (~1f%)~n",
-               [NL,T,B,BK,NS,SP,BD,NL,CP]).
+        SP is 100*BK/NS, CP is 100*BD/NL.
+
+mean(L,M) :- sum_list(L,S), length(L,N), M is S/N.
+
+main :- set_random(seed(1)), statistics(cputime,T0),
+        findall(B-(SP-CP), (between(1,20,_), best100(B,SP,CP)), Rs),
+        statistics(cputime,T1), T is T1-T0,
+        findall(B,member(B-_,Rs),Bs),       mean(Bs,MB),
+        findall(SP,member(_-(SP-_),Rs),SPs), mean(SPs,MS),
+        findall(CP,member(_-(_-CP),Rs),CPs), mean(CPs,MC),
+        aggregate_all(count,leaf(_),NL),
+        format("decisions ~w | ~1fs | mean d ~3f | mean soft ~1f% | mean cost ~1f%~n",
+               [NL,T,MB,MS,MC]).
