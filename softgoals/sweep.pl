d0384d6ee7d0e69a1025dd434a54d3da0665ed91
timm <timm@ieee.org>
2026-07-27 21:23:33 -0400

    softgoals: sweep at N=256, report decisions and best pair

--- softgoals/sweep.pl
+++ softgoals/sweep.pl
@@ -2,7 +2,7 @@
 % Scores: soft = satisficed softgoals / softgoals (want 1);
 % cost = leaves assumed satisfied / leaves (want 0);
 % d = sqrt((1-soft)^2 + cost^2)/sqrt(2), 0..1, lower better.
-% usage: swipl sweep.pl models/CSServices.pl
+% usage: swipl sweep.pl models/CSServices.pl   (N=256 worlds)
 :- ['nfr2.pl'].
 :- assertz(greedy).
 :- initialization(main, main).
@@ -28,15 +28,12 @@ d(K,D,Dist) :- aggregate_all(count,node(_,softgoal),NS),
                Dist is sqrt((1-S)^2 + C^2)/sqrt(2).
 
 main :- set_random(seed(1)), statistics(cputime,T0),
-        findall(K-D, (between(1,800,_), world(K,D)), Ws),
+        findall(K-D, (between(1,256,_), world(K,D)), Ws),
         statistics(cputime,T1), T is T1-T0,
-        forall(member(N,[50,100,200,400,800]),
-               ( length(P,N), append(P,_,Ws),
-                 findall(Dist,(member(K-D,P),d(K,D,Dist)),Ds),
-                 min_list(Ds,B), format(" N~w ~3f |",[N,B]) )),
         findall(Dist-(K-D),(member(K-D,Ws),d(K,D,Dist)),Sc),
-        msort(Sc,[_-(BK-BD)|_]),
+        msort(Sc,[B-(BK-BD)|_]),
         aggregate_all(count,node(_,softgoal),NS),
         aggregate_all(count,leaf(_),NL),
         SP is 100*BK/NS, CP is 100*BD/NL,
-        format(" ~1fs | best: soft ~1f% cost ~1f%~n",[T,SP,CP]).
+        format("decisions ~w | ~1fs | d ~3f | soft ~w/~w (~1f%) | cost ~w/~w (~1f%)~n",
+               [NL,T,B,BK,NS,SP,BD,NL,CP]).
