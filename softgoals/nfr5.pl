5fe8d3e7f2022a1ddaa431f2eb7db05ab7d7397f
timm <timm@ieee.org>
2026-08-14 13:29:42 -0400

    sompler

--- softgoals/nfr5.pl
+++ softgoals/nfr5.pl
@@ -1,21 +1,32 @@
 :- op(1001,xfx,<--).
+:- multifile (<--)/2.
+:- dynamic (<--)/2.
 :- discontiguous (<--)/2.
 
-permute(Xs,Ys) :- random_permutation(Xs,Ys).
+any(Xs,X)      :- length(Xs,N), I is random(N), nth0(I,Xs,X).
+all([],[]).
+all(Xs,[Y|Ys]) :- any(Xs,Y), selectchk(Y,Xs,Zs), all(Zs,Ys).
 
-klauses(Head, Tails) :-
-  findall(Head/B, (Head <-- B), Pairs),
-  permute(Pairs, Tails).
+how(X=V,      chk(Z,V), L) :- memberchk(X=Z,L).
+how(X=V,      add(X=V), _).
+how([],       [],       _).
+how([H|T],    [H|T],    _).
+how(and(Xs),  Ys,       _) :- all(Xs,Ys).
+how(or(Xs),   [X],      _) :- any(Xs,X).
+how(makes(X), [X=t],    _).
+how(breaks(X),[X=f],    _).
+how(helps(X), [X=V],    _) :- any([t,t,f],V).
+how(hurts(X), [X=V],    _) :- any([f,f,t],V).
+how(must(X),  [X, X=t], _).   % hard goal: derive it, then insist
+how(X,        [],       L) :- memberchk(X=_,L).
+how(X,        (X <-- [B|Bs]),_) :- findall(B0, (X <-- B0),[B|Bs]).
+how(X,        add(X=t), _) :- atom(X).
 
-\+(X,Y,L,L) :- \+ member(X=Y,L).
+isamp1([],         L, L).
+isamp1(chk(V,V),   L, L).
+isamp1(add(B),     L, [B|L]).
+isamp1([H|T],      L0,L) :- isamp(H,L0,L1), isamp(T,L1,L).
+isamp1((X <-- Bs), L0,L) :-
+  ( isamp(or(Bs),[X=t|L0],L) -> true ; atom(X), L = [X=f|L0] ).
 
-=(X,Y,L0,L0) :- member(X=Z,L0), !, Y=Z.
-=(X,Y,L,[X=Y|L]).
-
-prove(X=Y)        --> X=Y.
-prove(not(X=Y))   --> \+(X,Y).
-prove(and(L))     --> {permute(L,L1)}, prove(all(L1)).
-prove(or(L))      --> {permute(L,L1), member(X,L1)}, prove(X).
-prove(all([]))    --> [].
-prove(all([H|T])) --> prove(H), prove(all(T)).
-prove(X)          --> {klauses(X,Ys), member(X/B,Ys)}, prove(B).
+isamp(X,L0,L) :- once(how(X,W,L0)), isamp1(W,L0,L).
