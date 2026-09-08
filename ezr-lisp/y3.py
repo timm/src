51382002677898bba9689bc2f2f9e698b3dec114
timm <timm@ieee.org>
2026-09-07 20:36:36 -0400

    dasdas

--- ezr-lisp/y3.py
+++ ezr-lisp/y3.py
@@ -368,14 +368,14 @@ def run(f=None):
   except Exception: traceback.print_exc(); return 1
   return 0
 
-def cli(d, args, n=0):
+def cli(d, funs, args, n=0):
   while args:
     s = args.pop(0)
-    if   s[:2] == "--": n += run(globals().get("test_"+s[2:]))
+    if   s[:2] == "--": n += run(funs().get("test_"+s[2:]))
     elif s[0] == "-" and s[1:] in d:
       d[s[1:]] = atom(args.pop(0))
     else: print(f"unknown arg: {s}")
   sys.exit(n)
 
 if __name__ == "__main__":
-  cli(vars(the), sys.argv[1:] or ["--help"])
+  cli(vars(the), globals(), sys.argv[1:] or ["--help"])
